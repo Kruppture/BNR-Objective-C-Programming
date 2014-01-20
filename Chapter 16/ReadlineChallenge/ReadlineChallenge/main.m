46c5507cb4585922825e0be10250dacad7e0046d
Konnor Krupp <krupp2014@gmail.com>
2014-01-20 18:41:09 -0500

    Initial Commit

---
+++ Chapter 16/ReadlineChallenge/ReadlineChallenge/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ReadlineChallenge
+//
+//  Created by Konnor Krupp on 1/20/14.
+//  Copyright (c) 2014 Konnor Krupp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        NSLog(@"nigga ima fuck you up");
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
